9cba4d7cfde5fe9f7822382d5b6931ee6ef41402
Sofia Ferrer <chopotolina@gamil.com>
2021-07-16 20:10:15 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
 function App() {
+  // guardo url en una constante
+  const url = 'https://some-random-api.ml/animal/cat'
+
+  const [animal, setAnimal] = useState('')
+
+  //busco data en api
+  const fetchApi = async () => {
+    const response = await fetch(url)
+      .then(response => response.json())
+      .then(json => setAnimal(json));
+  }
+
+  //funcion que recarga datos
+  const cargarAnimal = () => {
+    fetchApi()
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contanier">
+        <img src={animal.image} alt="imagen" className="animal-img" />
+        <p className="animal-fact">{animal.fact}</p>
+        <button className="button" onClick={() => cargarAnimal()}>CARGAR</button>
+      </div>
     </div>
   );
 }
